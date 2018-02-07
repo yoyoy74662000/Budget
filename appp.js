1a71aa16bcc02341d032604b504b46eb7bc4c810
yoyoy74662000 <yoyoy74662000@github.com>
2018-02-07 16:02:34 -0600

    Demo

--- appp.js
+++ appp.js
@@ -38,9 +38,23 @@ var UIController = (function () {
 // Global app controller
 var controller = (function(budgetCtrl, UICtrl){
 
-
+    var setupEventListener = function () {
 
     var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+
+            if(event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+
+        });
+
+    };
+
+
 
     var ctrlAddItem = function () {
 
@@ -48,18 +62,19 @@ var controller = (function(budgetCtrl, UICtrl){
         var input = UICtrl.getinput();
         console.log(input);
 
-    }
+    };
 
-    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
-
-    document.addEventListener('keypress', function (event) {
-
-        if(event.keyCode === 13 || event.which === 13){
-            ctrlAddItem();
+    return {
+        init: function () {
+            console.log('Application Started')
+            setupEventListener();
         }
+    };
 
-    });
+
 
 
 
 })(budgetController, UIController);
+
+controller.init();
