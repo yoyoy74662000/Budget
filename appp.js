b5bb0e4c0025f3f675a5f42dcfed98f7d7ff7bab
yoyoy74662000 <yoyoy74662000@github.com>
2018-02-07 01:02:47 -0600

    Demo

--- appp.js
+++ appp.js
@@ -10,25 +10,43 @@ var budgetController = (function () {
 var UIController = (function () {
 
 
+    return {
+        getinput: function () {
+
+            return {
+
+                type: document.querySelector('.add__type').value,
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+
+            };
+
+        }
+    };
+
 
 })();
 
 
 // Global app controller
 var controller = (function(budgetCtrl, UICtrl){
-    document.querySelector('.add__btn').addEventListener('click',function () {
+    
+    var ctrlAddItem = function () {
+
+        // 1. get the field of input data
+        var input = UICtrl.getinput();
+        console.log(input);
+
+    }
+    
+    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
 
 
-
-    });
 
     document.addEventListener('keypress', function (event) {
 
         if(event.keyCode === 13 || event.which === 13){
-            console.log('Enter was Pressed')
-        }else{
-            console.log('Other key was Pressed')
-
+            ctrlAddItem();
         }
 
     });
